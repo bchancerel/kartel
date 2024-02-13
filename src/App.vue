92568b0c5a280e43938d5ac1fc804b1154e3f9e9
bchancerel <benjamin.chancerel@outlook.com>
2024-02-13 15:24:44 +0100

    FIX: hide footerbar

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,16 @@
       <NavBar/>
     </nav>
     <router-view/>
-    <FooterBar/>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
-import FooterBar from './components/FooterBar.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar,
-    FooterBar
+    NavBar
   }
 }
 </script>
